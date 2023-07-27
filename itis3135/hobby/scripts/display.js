0a849dcb3a6ab1b2f5487f5d0dc25a39381eac72
SwiftSimmer <90529223+SwiftSimmer@users.noreply.github.com>
2023-07-26 20:41:34 -0400

    fixed title

---
+++ itis3135/hobby/scripts/display.js
@@ -0,0 +1,22 @@
+window.onload = function () {
+    document.getElementById("genre_response").onclick = genreResponse;
+};
+
+function openTab(event, tabNum) {
+
+    var i;
+    var tabcontent;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tabButton = document.getElementsByClassName("tabButton");
+    for (i = 0; i < tabButton.length; i++) {
+        tabButton[i].className = tabButton[i].className.replace(" active", "");
+    }
+
+    document.getElementById(tabNum).style.display = "block";
+    event.currentTarget.className += " active";
+}
